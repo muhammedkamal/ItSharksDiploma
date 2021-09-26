5e826eb73b9bda99b1587d88b1d9de9fc51630e0
muhammad kamal <muhammedkamal022@gmail.com>
2021-09-26 10:52:07 +0200

    numbers data types

---
+++ numbers.dart
@@ -0,0 +1,28 @@
+/**
+ * int var_name;      // declares an integer variable 
+ * double var_name;   // declares a double variable 
+ */
+void main() {
+  // that will work
+  int num1 = 10;
+  double num2 = 10.50;
+  print(num1);
+  print(num2);
+
+  // that also would work
+  print(num.parse('12'));
+  print(num.parse('10.91'));
+
+  // that aslo would work
+  String num3 = "211";
+  print(int.parse(num3));
+
+  // that will not work
+  // print(num.parse('12A'));   run time error if you uncommented that
+
+  // what if I want only 3 numbers after decimal point ??
+  // easy besy
+  double oneThird = 1 / 3;
+  print(oneThird);
+  print(oneThird.toStringAsFixed(3));
+}
