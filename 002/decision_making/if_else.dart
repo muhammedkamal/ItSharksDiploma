9a8dbc137438c2d4732b5e97cd5824aa24d50875
muhammad kamal <muhammedkamal022@gmail.com>
2021-09-27 13:47:07 +0200

    if ..else

---
+++ 002/decision_making/if_else.dart
@@ -0,0 +1,27 @@
+/**
+ * if (boolean_expression1) { 
+    //statements if the expression1 evaluates to true 
+  } 
+  else if (boolean_expression2) { 
+    //statements if the expression2 evaluates to true 
+  } 
+  else { 
+    //statements if both expression1 and expression2 result to false 
+  } 
+ */
+void main() {
+  var num = 2;
+  if (num > 0) {
+    print("${num} is positive");
+  } else if (num < 0) {
+    print("${num} is negative");
+  } else {
+    print("${num} is neither positive nor negative");
+  }
+  // type checking
+  if (num.runtimeType == int) {
+    print("integer");
+  } else {
+    print("double");
+  }
+}
