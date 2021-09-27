91e19369a91757f3989b7b8c919b92aabcd380e8
muhammad kamal <muhammedkamal022@gmail.com>
2021-09-27 17:02:06 +0200

    polymorphism

--- 002/classes/vehicle.dart
+++ 002/classes/vehicle.dart
@@ -24,3 +24,30 @@ class Vehicle {
     print("$make moved $distance in $direction");
   }
 }
+
+// inheritance
+class Car extends Vehicle {
+  final String make;
+  final int model;
+  final String color;
+  Status _VehicleStatus = Status.stopped;
+  Car({required this.make, required this.model, required this.color})
+      : super(make: make, model: model, color: color);
+
+  // Polymorphism
+  @override
+  void move(int distance, String direction) {
+    print("this car is moving");
+    super.move(distance, direction);
+  }
+}
+
+void main() {
+  // this how you declare an object of type class
+  Car car = Car(make: "Honda", model: 1999, color: "Black");
+  print(car.VehicleStatus);
+  //this how to use setters
+  car..status = Status.running;
+  print(car.VehicleStatus);
+  car.move(20, "East");
+}
