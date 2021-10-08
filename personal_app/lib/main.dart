90cbbbb56a577fc02fc746139101add4e4206ac1
muhammad kamal <muhammedkamal022@gmail.com>
2021-10-08 16:59:45 +0200

    add url launchers

--- personal_app/lib/main.dart
+++ personal_app/lib/main.dart
@@ -47,13 +47,13 @@ class Home extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          CircleAvatar(
+        children: [
+          const CircleAvatar(
             minRadius: 50,
             maxRadius: 150,
             backgroundImage: AssetImage("assets/images/personal.jpeg"),
           ),
-          Padding(
+          const Padding(
             padding: EdgeInsets.all(8.0),
             child: Text(
               "MUHAMMAD KAMAL",
@@ -65,7 +65,7 @@ class Home extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
           ),
-          Padding(
+          const Padding(
             padding: EdgeInsets.all(8.0),
             child: Text(
               "Software Engineer",
@@ -81,18 +81,24 @@ class Home extends StatelessWidget {
           ContactCard(
             contactInfo: '+201555786787',
             icon: Icons.phone,
+            onTap: () => _launchURL('tel:+201555786787'),
           ),
           ContactCard(
             contactInfo: 'muhammedkamal022@gmail.com',
             icon: Icons.email,
+            onTap: () => _launchURL(
+                'mailto:muhammedkamal022@gmail.com?subject=App%20Contact&body='),
           ),
           ContactCard(
             contactInfo: 'muhammedkamal',
             icon: FontAwesome.github,
+            onTap: () => _launchURL('https://github.com/muhammedkamal'),
           ),
           ContactCard(
             contactInfo: 'muhammedkamal022',
             icon: FontAwesome.facebook,
+            onTap: () =>
+                _launchURL('https://www.facebook.com/Muhammadkamal022/'),
           ),
         ],
       ),
@@ -103,11 +109,16 @@ class Home extends StatelessWidget {
 class ContactCard extends StatelessWidget {
   final String contactInfo;
   final IconData icon;
-  const ContactCard({required this.contactInfo, required this.icon});
+  final Function onTap;
+  ContactCard(
+      {required this.contactInfo, required this.icon, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTap: () {
+        onTap();
+      },
       child: Card(
         elevation: 10,
         color: Colors.blueGrey,
